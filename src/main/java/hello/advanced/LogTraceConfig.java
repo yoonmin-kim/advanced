44c1be2f578fa605fa755d97c76ea7681009193b
yoonmin-kim <rladbsals23@gmail.com>
2021-11-07 14:47:11 +0900

    feat: ThreadLocal 적용 및 주의사항
    적용:
    - 컨트롤러, 서비스, 레파지토리에는 LogTrace 인터페이스를 정의하고
      주입받게끔 되어있다
    - 따라서 LogTraceConfig 에 정의한 Bean 만 ThreadLocalLogTrace 로 바꿔주면 된다
    
    주의점:
    - Thread 의 생성비용이 크기때문에 일반적으로 WAS 는 Thread 풀을 사용한다
    - 때문에 Thread 가 재사용 되면서 ThreadLocal 의 값이 제거되지 않고 재사용 될
      위험이 있다
    - 모든 요청이 끝나면 필터나 인터셉터를 활용하여 ThreadLocal.remove() 를 꼭 처리해주자

--- src/main/java/hello/advanced/LogTraceConfig.java
+++ src/main/java/hello/advanced/LogTraceConfig.java
@@ -3,14 +3,14 @@ package hello.advanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import hello.advanced.app.trace.logtrace.FieldLogTrace;
 import hello.advanced.app.trace.logtrace.LogTrace;
+import hello.advanced.app.trace.logtrace.ThreadLocalLogTrace;
 
 @Configuration
 public class LogTraceConfig {
 
 	@Bean
 	public LogTrace logTrace() {
-		return new FieldLogTrace();
+		return new ThreadLocalLogTrace();
 	}
 }
