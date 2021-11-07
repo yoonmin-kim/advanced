7a114cdee4d8acdb5fc0c89c7bf8ef6da4bb66b6
yoonmin-kim <rladbsals23@gmail.com>
2021-11-07 14:33:27 +0900

    feat: ThreadLocal 동기화 - 개발

---
+++ src/test/java/hello/advanced/app/trace/logtrace/ThreadLocalLogTraceTest.java
@@ -0,0 +1,26 @@
+package hello.advanced.app.trace.logtrace;
+
+import org.junit.jupiter.api.Test;
+
+import hello.advanced.app.trace.TraceStatus;
+
+class ThreadLocalLogTraceTest {
+
+	ThreadLocalLogTrace trace = new ThreadLocalLogTrace();
+
+	@Test
+	void begin_end_level2() {
+		TraceStatus status1 = trace.begin("hello1");
+		TraceStatus status2 = trace.begin("hello2");
+		trace.end(status2);
+		trace.end(status1);
+	}
+
+	@Test
+	void begin_exception_level2() {
+		TraceStatus status1 = trace.begin("hello1");
+		TraceStatus status2 = trace.begin("hello2");
+		trace.exception(status2, new IllegalStateException());
+		trace.exception(status1, new IllegalStateException());
+	}
+}
