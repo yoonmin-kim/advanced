04500a0871365abe8b217feee529c889f5f3f1f4
yoonmin-kim <rladbsals23@gmail.com>
2021-11-10 21:01:15 +0900

    feat: 템플릿 메서드 패턴 - 예제3
    예제2 방식은 로직이 바뀔때 마다 클래스를 상속받고
    새로 작성해야하는 단점이 있다
    
    익명 내부클래스를 사용하여 문제를 해결할 수 있다

--- src/test/java/hello/advanced/app/trace/template/TemplateMethodTest.java
+++ src/test/java/hello/advanced/app/trace/template/TemplateMethodTest.java
@@ -46,4 +46,23 @@ public class TemplateMethodTest {
 		AbstractTemplate template2 = new SubClassLogic2();
 		template2.execute();
 	}
+
+	@Test
+	void templateMethodV2() {
+		AbstractTemplate template1 = new AbstractTemplate() {
+			@Override
+			protected void call() {
+				log.info("비즈니스 로직1 실행");
+			}
+		};
+		template1.execute();
+
+		AbstractTemplate template2 = new AbstractTemplate() {
+			@Override
+			protected void call() {
+				log.info("비즈니스 로직2 실행");
+			}
+		};
+		template2.execute();
+	}
 }
