89a14ae9163722e4ec196777434c16c9dd180bfb
yoonmin-kim <rladbsals23@gmail.com>
2021-11-14 13:33:46 +0900

    feat: 전략 패턴 - 예제3
    ContextV1 에서는 전략을 필드로 보관하는(선조립 후실행) 방식이였다.
    ContextV2 는 전략을 파라미터로 전달받는 방식으로 execute() 실행시 전략을
    전달 받아서 사용한다

--- src/test/java/hello/advanced/app/trace/strategy/code/strategy/ContextV2.java
+++ src/test/java/hello/advanced/app/trace/strategy/code/strategy/ContextV2.java
@@ -3,18 +3,12 @@ package hello.advanced.app.trace.strategy.code.strategy;
 import lombok.extern.slf4j.Slf4j;
 
 /**
- * 전약을 필드로 전달받는 방식
+ * 전약을 파라미터로 전달받는 방식
  */
 @Slf4j
-public class ContextV1 {
+public class ContextV2 {
 
-	private Strategy strategy;
-
-	public ContextV1(Strategy strategy) {
-		this.strategy = strategy;
-	}
-
-	public void execute() {
+	public void execute(Strategy strategy) {
 		long startTime = System.currentTimeMillis();
 		// 비즈니스 로직 실행
 		strategy.call(); // 위임
